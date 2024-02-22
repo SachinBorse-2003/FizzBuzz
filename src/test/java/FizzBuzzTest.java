15fde1294b86b7a41902c6089a028c337082e3b3
Sachin <Sachinkailasborse@gmail.com>
2024-02-22 11:12:30 +0530

    create First Test

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,3 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
 public class FizzBuzzTest {
-    
+
+    @Test
+    public void testInstanceCreation(){
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        assertNotNull(fizzbuzz);
+    }
+
+
 }
